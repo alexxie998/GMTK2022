a25b3abbf4d64a2ad062a11d77c9cf9b6cf8e444
alexxie998 <52658259+alexxie998@users.noreply.github.com>
2022-07-15 22:20:38 -0700

    Random Tree generation

---
+++ Scripts/generateObstacles.gd
@@ -0,0 +1,61 @@
+extends TileMap
+
+var rng = RandomNumberGenerator.new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var illegalCoords = []
+
+func holeCoords(vect):
+	illegalCoords.append(vect)
+	illegalCoords.append(Vector2(vect.x, vect.y+1))
+	illegalCoords.append(Vector2(vect.x+1, vect.y))
+	illegalCoords.append(Vector2(vect.x+1, vect.y+1))
+	
+	
+func generateBlocks():
+	rng.randomize()
+	var randx = rng.randi_range(1, 30)
+	var randy = rng.randi_range(1, 30)
+	#print("block at ")
+	#print(randx)
+	#print(randy)
+	if !(illegalCoords.has(Vector2(randx, randy))):
+		#print("legal")
+		illegalCoords.append(Vector2(randx, randy))
+		set_cell(randx,randy, tile_set.find_tile_by_name("Tree"))
+		#print("success")
+		return Vector2(randx, randy)
+	#else:
+		#print("illegal")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	holeCoords(Vector2(5,5))
+	holeCoords(Vector2(11,11))
+	holeCoords(Vector2(19,19))
+	holeCoords(Vector2(25,25))
+	holeCoords(Vector2(25,5))
+	holeCoords(Vector2(11,19))
+	holeCoords(Vector2(19,11))
+	holeCoords(Vector2(5,25))
+	
+	for x in 31:
+		illegalCoords.append(Vector2(x, 0))
+		illegalCoords.append(Vector2(x, 31))
+
+	for y in 30:
+		illegalCoords.append(Vector2(0, y))
+		illegalCoords.append(Vector2(31, y))
+		
+	for n in 300:
+		generateBlocks()
+		
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
